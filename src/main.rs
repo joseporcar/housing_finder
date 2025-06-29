135ae386d01e6273456352493a10149db0e8f288
joseporcar <joseaporcar@gmail.com>
2025-06-29 16:29:54 -0400

    removed tokio, no need for async

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod send_email;
 use crate::send_email::Mailer;
+
 fn main() {
     dotenvy::dotenv().expect("There is an issue with the .env file");
-    let mailer = Mailer::new();
+    // let mailer = Mailer::new();
 
-    mailer.send_email("there is housing in your area");
+    // mailer.send_email("there is housing in your area");
+
 }
