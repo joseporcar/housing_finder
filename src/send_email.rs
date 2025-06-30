0afd307aebe4b8c3db97d80d60f8c3de26c1375b
joseporcar <joseaporcar@gmail.com>
2025-06-29 22:41:33 -0400

    ran cargo fmt

--- src/send_email.rs
+++ src/send_email.rs
@@ -1,9 +1,9 @@
-use std::env;
 use lettre::address::AddressError;
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
+use std::env;
 
-#[derive(Debug)] 
+#[derive(Debug)]
 pub struct Mailer {
     sender: String,
     recipient: String,
@@ -12,24 +12,35 @@ pub struct Mailer {
 
 impl Mailer {
     pub fn new() -> Self {
-        Self { sender: env::var("SENDER_EMAIL").unwrap(), 
-            recipient: env::var("RECIPIENT_EMAIL").unwrap() ,
-            relay: SmtpTransport::relay("smtp.gmail.com").unwrap().credentials(Credentials::new(env::var("SENDER_EMAIL").unwrap(), env::var("PASSWORD").unwrap())).build()
+        Self {
+            sender: env::var("SENDER_EMAIL").unwrap(),
+            recipient: env::var("RECIPIENT_EMAIL").unwrap(),
+            relay: SmtpTransport::relay("smtp.gmail.com")
+                .unwrap()
+                .credentials(Credentials::new(
+                    env::var("SENDER_EMAIL").unwrap(),
+                    env::var("PASSWORD").unwrap(),
+                ))
+                .build(),
         }
     }
 
     fn message(&self, content: &str) -> Result<Message, AddressError> {
         Ok(Message::builder()
-                    .from(self.sender.parse()?)
-                    .to(self.recipient.parse()?)
-                    .subject("New housing found!")
-                    .body(content.to_owned()).unwrap())
+            .from(self.sender.parse()?)
+            .to(self.recipient.parse()?)
+            .subject("New housing found!")
+            .body(content.to_owned())
+            .unwrap())
     }
 
     pub fn send_email(&self, content: &str) {
-        match self.relay.send(&self.message(content).expect("invalid email adress")) {
+        match self
+            .relay
+            .send(&self.message(content).expect("invalid email adress"))
+        {
             Ok(_) => println!("email sent"),
-            Err(e) => println!("error sending email: {:?}", e)
+            Err(e) => println!("error sending email: {:?}", e),
         }
     }
 }
